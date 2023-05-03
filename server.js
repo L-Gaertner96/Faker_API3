b9521597f62cf15130a33c0da03e0cb92df327b4
L-Gaertner96 <gaertner.logan@gmail.com>
2023-05-03 19:29:18 -0400

    .

---
+++ server.js
@@ -0,0 +1,56 @@
+const {faker} = require("@faker-js/faker")
+
+const express = require("express");
+const app = express();
+const port = 8000;
+
+const createUser = () => {
+    const user = {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.phoneNumber(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        _id: faker.datatype.uuid()
+    };
+    return user;
+};
+
+const createCompany = () => {
+    const company = {
+        _id: faker.datatype.uuid(),
+        name: faker.company.name(),
+        address: {
+            street: faker.address.streetAddress(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipcode: faker.address.zipCode(),
+            country: faker.address.country(),
+        },
+    };
+    return company;
+}
+// req is short for request
+// res is short for response
+app.get("/api/users/new", (req, res) => {
+    const newUser = createUser();
+    res.json(newUser);
+});
+
+app.get("/api/companies/new", (req, res) => {
+    const newCompany = createCompany();
+    res.json(newCompany);
+});
+
+app.get("/api/user/company", (req, res) => {
+    const newUser = createUser();
+    const newCompany = createCompany();
+    const userCompany = {
+        user: newUser,
+        company: newCompany,
+    };
+    res.json(userCompany);
+});
+
+// this needs to be below the other code blocks
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
